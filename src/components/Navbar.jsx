75978b1dfd8b1498d265d07dc3d2a8fdae4d7f15
Go'zal <gozalmominova6@gmail.com>
2024-11-19 01:36:08 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
           src="https://www.dezinfeksiyatashkent.uz/assets/dez_logo2-5b433e48.png"
           alt="header_icon"
         />
+        <h1>hello</h1>
       </nav>
     </header>
   );
